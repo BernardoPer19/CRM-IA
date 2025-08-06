45538d119bc58c751a2bc0f5ceb019c21cacf758
jhoaking <wawxper08@gmail.com>
2025-08-06 12:08:10 -0400

    add new CookieOption on production

--- backend/src/features/auth/utils/cookiesOptions.ts
+++ backend/src/features/auth/utils/cookiesOptions.ts
@@ -2,7 +2,7 @@ import type { CookieOptions } from "express";
 
 export const options: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000,
+    secure: true,           
+    sameSite: "none",      
+    maxAge: 1000 * 60 * 60 * 24 * 7, 
 };
