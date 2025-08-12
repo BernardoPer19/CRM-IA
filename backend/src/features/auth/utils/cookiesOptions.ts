bb5e5205d3733d57baf6b0e3da2b7ed31ef320f2
jhoaking <wawxper08@gmail.com>
2025-08-12 13:36:44 -0400

    add changes to production

--- backend/src/features/auth/utils/cookiesOptions.ts
+++ backend/src/features/auth/utils/cookiesOptions.ts
@@ -1,8 +1,8 @@
 import type { CookieOptions } from "express";
 
 export const options: CookieOptions = {
-   httpOnly: true,
-  secure: false, // en dev
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // true solo en producción
   sameSite: "lax",
   path: "/",
 };
