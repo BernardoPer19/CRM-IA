3d29966f1763b74f547e77917da2d302adc7761d
jhoaking <wawxper08@gmail.com>
2025-08-14 09:45:43 -0400

    add new factor cookie

--- backend/src/features/auth/utils/cookiesOptions.ts
+++ backend/src/features/auth/utils/cookiesOptions.ts
@@ -2,8 +2,8 @@ import type { CookieOptions } from "express";
 
 
 export const getCookieOptions = (): CookieOptions => ({
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // Solo en producción
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  path: "/",
+    httpOnly: true, // para seguridad
+    secure: process.env.NODE_ENV === "production", // en prod solo con HTTPS
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 días
 });
